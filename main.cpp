e52cd15e702dd7264ccc68be252da4f36bf20166
pdhanaraj89 <pradeepraj.16@gmail.com>
2018-06-21 12:41:36 +0530

    updates

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
-
+#include <string>
 #include <fstream>  
 
 
@@ -10,16 +10,19 @@ int main()
 	int i = 0; 
 	
 	std::ofstream outfile ("test.txt");
+	
+	std::string statement(" Minute - Hello I am still running. What do you want me to ?");
 
 	while(true)
 	{
 		std::this_thread::sleep_for (std::chrono::seconds(60));
 		i++;
-		std::cout<<i<<" th Minute- Hello I am still running. What do you want me to ? "<< std::endl;
-		outfile <<i<< " th Minute- Hello I am still running. What do you want me to ? "<< std::endl;
+		std::cout<<i<<statement<< std::endl;
+		outfile <<i<< statement<< std::endl;
 	}
 
 	outfile.close();
 	
 	return 0;
+	
 }
